221715a0a3e7481a6b2639562d14f4954db4f65d
Njoter <stianovesen@gmail.com>
2025-11-24 19:34:17 +0100

    Changed config variable names to lowercase

--- variables.sh
+++ variables.sh
@@ -5,12 +5,13 @@
 
 
 # === configs === #
-BASHRC="$HOME/.bashrc"
-BASHSETTINGS="$HOME/.bashrc.d/settings.sh"
-BASHALIASES="$HOME/.bashrc.d/aliases.sh"
-FUNCTIONS="$HOME/.bashrc.d/functions.sh"
-BASHVARIABLES="$HOME/.bashrc.d/variables.sh"
-VIMRC="$HOME/.vim/vimrc"
-HARPOONCONFIG="$HOME/.config/nvim/after/plugin/harpoon.lua"
-AWECONFIG="$HOME/.config/awesome/rc.lua"
-SWAYCONFIG="$HOME/.config/sway/config"
+bashrc=~/.bashrc.d/.bashrc
+bashsettings=~/.bashrc.d/settings.sh
+bashaliases=~/.bashrc.d/aliases.sh
+bashfunctions=~/.bashrc.d/functions.sh
+bashvariables=~/.bashrc.d/variables.sh
+vimrc=~/.vim/vimrc
+harpoonconfig=~/.config/nvim/after/plugin/harpoon.lua
+aweconfig=~/.config/awesome/rc.lua
+swayconfig=~/.config/sway/config
+tuxconfig=~/.config/tmux/tmux.conf
